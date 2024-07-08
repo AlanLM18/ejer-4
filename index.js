d5f82899d87f701ccad2a3a10b1de64d3f084724
AlanLM18 <alanlm.ti23@utsjr.edu.mx>
2024-07-08 14:35:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const path=require("path");
 const usuariosRutas=require("./routes/usuariosRutas");
 const app=express();
+const port = process.env.PORT || 3000;
 
 app.use("/",express.static(path.join(__dirname, "/web")));
 app.set("view engine","ejs");
